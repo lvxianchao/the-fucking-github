cb2110b74cee39c98d83b007dfafecd651f0c9a8
lxc <coderlxc@gmail.com>
2019-04-12 21:11:40 +0800

    Fixed the bug that user cannot open the extension when it be closed.

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,7 @@
+// The Fucking Github 的标签页 ID
+window.tabId = null;
+
+// 当点击了插件 LOGO 后判断是否已开启了插件页面，如果已经开启了，则激活插件页面，否则开启插件页面。
 chrome.browserAction.onClicked.addListener(function () {
     let index = chrome.extension.getURL('index.html');
 
@@ -7,5 +11,12 @@ chrome.browserAction.onClicked.addListener(function () {
         chrome.tabs.create({url: index}, function (tab) {
             window.tabId = tab.id;
         });
+    }
+});
+
+// 监听标签页关闭事件，如果关闭的是插件页面，则重置插件的标签页 ID。
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    if (tabId === window.tabId) {
+        window.tabId = null;
     }
 });
